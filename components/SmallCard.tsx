52f4b52a38549f902dd1688932bde4088487fb7e
Cagri Oz <cagriozarpaciii@hotmail.com>
2022-07-08 12:11:51 +0300

    Explore cards done

---
+++ components/SmallCard.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const SmallCard = ({ img, location, distance }: { img: string; location: string; distance: string }) => {
+    return (
+        <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition duration-200 ease-in-out">
+            {/* Left */}
+            <div className="relative h-16 w-16">
+                <Image src={img} layout="fill" className="rounded-lg" />
+            </div>
+
+            {/* Right */}
+            <div>
+                <h2>{location}</h2>
+                <h3 className="text-gray-500">{distance}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default SmallCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import Banner from '../components/Banner';
-import Header from '../components/Header';
+import Head from "next/head";
+import Image from "next/image";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
 
-const Home: NextPage = () => {
+interface ExploreData {
+    img: string;
+    location: string;
+    distance: string;
+}
+
+interface CardsData {
+    img: string;
+    title: string;
+}
+
+const Home = ({ exploreData }: { exploreData: ExploreData[] }) => {
     return (
         <div className="">
             <Head>
@@ -15,13 +26,36 @@ const Home: NextPage = () => {
             <Header />
             <Banner />
 
-            <main>
+            <main className="max-w-7xl mx-auto px-8 sm:px-16">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+                    {/* Pull some data from server - API endpoints */}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        {exploreData?.map(({ img, distance, location }) => (
+                            <SmallCard key={img} img={img} location={location} distance={distance} />
+                        ))}
+                    </div>
+                </section>
+
                 <section>
-                    <h2 className="text-4xl font-semibold">Explore Nearby</h2>
+                    <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
                 </section>
             </main>
         </div>
     );
 };
+
+export async function getStaticProps() {
+    const exploreData: ExploreData[] = await fetch("https://links.papareact.com/pyp").then((res) => res.json());
+
+    const cardsData: CardsData[] = await fetch("https://links.papareact.com/pyp").then((res) => res.json());
+
+    return {
+        props: {
+            exploreData,
+        },
+    };
+}
 
 export default Home;
