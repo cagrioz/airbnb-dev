a646cd206a40abf7d590ef3ca6e32426173c09d5
Cagri Oz <cagriozarpaciii@hotmail.com>
2022-07-12 22:55:59 +0300

    map

--- components/Map.tsx
+++ components/Map.tsx
@@ -7,16 +7,20 @@ import { getCenter } from "geolib";
 import { SearchItem, Coord } from "../interfaces";
 
 const Map = ({ coords, searchResults }: { coords: Coord[]; searchResults: SearchItem[] }): JSX.Element => {
+    const [selectedLocation, setSelectedLocation] = useState<Coord | Object>({});
+
     // Get the center, if there are no coords, return 0, 0
     const center: Coord = getCenter(coords) || { latitude: 0, longitude: 0 };
 
     const [viewport, setViewport]: [any, (viewport: any) => void] = useState({
         width: "100%",
         height: "100%",
-        latitude: center?.latitude - 0.15,
+        latitude: center?.latitude - 0.1,
         longitude: center?.longitude,
         zoom: 11,
     });
+
+    console.log(selectedLocation);
 
     return (
         <ReactMapGL
@@ -28,8 +32,27 @@ const Map = ({ coords, searchResults }: { coords: Coord[]; searchResults: Search
             {searchResults.map((result) => (
                 <div key={result.img}>
                     <Marker latitude={result.lat as number} longitude={result.long as number} anchor="bottom">
-                        <p className="cursor-pointer text-2xl animate-bounce">📌</p>
+                        <p
+                            role="img"
+                            onClick={() => setSelectedLocation(result)}
+                            className="cursor-pointer text-2xl animate-bounce"
+                            aria-label="push-pin"
+                        >
+                            📌
+                        </p>
                     </Marker>
+
+                    {selectedLocation === result && (
+                        <Popup
+                            latitude={result.lat as number}
+                            longitude={result.long as number}
+                            closeButton={true}
+                            closeOnClick={false}
+                            onClose={() => setSelectedLocation({})}
+                        >
+                            <h3>{result.title}</h3>
+                        </Popup>
+                    )}
                 </div>
             ))}
         </ReactMapGL>
